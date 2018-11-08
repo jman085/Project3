780fd195918c6b10d429107bad2c8b2111b83cde
Patrick Haimbaugh <Phaimbaugh2000@gmail.com>
2018-11-08 14:15:12 -0500

    Client and Server Run forever

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -96,8 +96,10 @@ final class ChatClient {
     private final class ListenFromServer implements Runnable {
         public void run() {
             try {
-                String msg = (String) sInput.readObject();
-                System.out.print(msg);
+                while(true) {
+                    String msg = (String) sInput.readObject();
+                    System.out.print(msg);
+                }
             } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
